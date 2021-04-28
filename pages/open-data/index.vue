deff8fe9f1b699e9e5039b44f7ed364ba744267e
Abd Dzuljalali Wal Ikram <dzuljalali.walikram@provisms.com>
2021-04-28 23:16:52 +0800

    Update design in open data

--- pages/open-data/index.vue
+++ pages/open-data/index.vue
@@ -11,13 +11,34 @@
 
     <!-- Blog Tiles -->
     <div class="open-data-tiles">
-      <tile
+      <div
+        v-for="(tile, i) in $t('pages.openData.tiles')"
+        :key="i"
+        class="open-data-tile"
+      >
+        <img
+          :src="require(`@/assets/images/${tile.img}`)"
+          alt="data"
+          class="open-data-tile__image"
+        />
+
+        <h1 class="open-data-tile__title">
+          <a :href="tile.link" target="_blank">
+            {{ tile.title }}
+          </a>
+        </h1>
+
+        <p class="open-data-tile__body">
+          {{ tile.body }}
+        </p>
+      </div>
+      <!-- <tile
         v-for="(tile, i) in $t('pages.openData.tiles')"
         :img="require(`@/assets/images/${tile.img}`)"
         :title="tile.title"
         :body="tile.body"
         :key="i"
-      />
+      /> -->
     </div>
   </div>
 </template>
