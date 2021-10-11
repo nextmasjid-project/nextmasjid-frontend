5e7aa5b98ebfc9e9576f638f2df023a3be7d2f80
Abd Dzuljalali Wal Ikram <35287914+kalamangna@users.noreply.github.com>
2021-10-11 21:44:30 +0800

    Component changes

--- pages/help/index.vue
+++ pages/help/index.vue
@@ -3,15 +3,20 @@
     <!-- Sub Hero -->
     <div class="help-sub-hero">
       <sub-hero
-          background-image="http://placehold.it/1920x768"
-          :title="$t('pages.help.subHero.title')"
-          :text="$t('pages.help.subHero.description')"/>
+        background-image="http://placehold.it/1920x768"
+        :title="$t('pages.help.subHero.title')"
+        :text="$t('pages.help.subHero.description')"
+      />
     </div>
 
     <!-- Top Cards -->
     <div class="help-top">
       <div class="help-top__list">
-        <div class="help-top__item" v-for="card in $t('pages.help.topTiles')">
+        <div
+          class="help-top__item"
+          v-for="card in $t('pages.help.topTiles')"
+          :key="card.title"
+        >
           <card :title="card.title" :body="card.body" />
         </div>
       </div>
@@ -19,11 +24,20 @@
 
     <!-- Accordion -->
     <div class="help-accordion">
-      <h2 class="help-accordion__heading">{{ $t('pages.help.accordion.title') }}</h2>
-      <accordion v-for="(item, index) in  $t('pages.help.accordion.list')" :key="`item-${index}`">
+      <h2 class="help-accordion__heading">
+        {{ $t("pages.help.accordion.title") }}
+      </h2>
+      <accordion
+        v-for="(item, index) in $t('pages.help.accordion.list')"
+        :key="`item-${index}`"
+      >
         <template slot="head" slot-scope="props">
           <h4 class="help-accordion__title">{{ item.title }}</h4>
-          <icon class="help-accordion__icon" size="small" :symbol="props.isOpen ? 'icon-minus' : 'icon-plus'" />
+          <icon
+            class="help-accordion__icon"
+            size="small"
+            :symbol="props.isOpen ? 'icon-minus' : 'icon-plus'"
+          />
         </template>
         <template slot="body">
           <div class="help-accordion__body">
@@ -32,24 +46,23 @@
         </template>
       </accordion>
     </div>
-
   </div>
 </template>
 
 <script>
-  export default {
-    name: "index",
-    head() {
-      return {
-        title: this.$t('meta.help.title'),
-        meta: [
-          {
-            hid: 'description',
-            name: 'description',
-            content: this.$t('meta.help.desc')
-          }
-        ]
-      }
-    }
+export default {
+  name: "index",
+  head() {
+    return {
+      title: this.$t("meta.help.title"),
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: this.$t("meta.help.desc")
+        }
+      ]
+    };
   }
+};
 </script>
